df3c105ace04327b450c0c265753c98cab219f77
Winda Pratiwi <windapratiwi12@gmail.com>
2021-02-26 09:52:42 +0700

    Menambah File aktifitas3.py

---
+++ aktifitas3.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 09:44:09 2021
+
+@author: Winda Pratiwi
+"""
+
+#PERCABANGAN
+
+print ("Percabangan pada Bahasa Pemrograman Python")
+print ("Pilih Bahasa Pemrograman: ")
+print ("1. Pythohn")
+print ("2. C")
+
+pilih= int (input("Pilih Menu (1/2) : "))
+
+if pilih == 1:
+    print ("Bahasa Pemrograman yang Dipilih adalah Python")
+elif pilih == 2:
+    print ("Bahasa Pemrograman yang Dipilih adalah C")
+else :
+    print ("Pilihan Anda tidak tersedia!!!")
