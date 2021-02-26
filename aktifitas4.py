12e6038e9c98bada2bd9d92d7ae444c6f11489fa
Winda Pratiwi <windapratiwi12@gmail.com>
2021-02-26 10:10:17 +0700

    Menambah File aktifitas4.py

---
+++ aktifitas4.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 10:03:06 2021
+
+@author: Winda Pratiwi
+"""
+
+print ("Contoh Perulangan Suatu String")
+for huruf in "SISTEL":
+    print (huruf)
+
+nilai_awal= 0
+nilai_akhir= 30 #tujuan/berapa kali
+step= 3 #jeda antar loop
+
+print ("Contoh Perulangan Angka Dengan Range ()")
+for i in range (nilai_awal, nilai_akhir, step):
+    print (i)
