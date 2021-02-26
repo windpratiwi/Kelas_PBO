fed6944c9986ba99d139efcf0620c5e6f82a51d3
Winda Pratiwi <windapratiwi12@gmail.com>
2021-02-26 09:13:19 +0700

    menambahkan file aktifitas1.py

---
+++ aktifitas1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 09:06:44 2021
+
+@author: Winda Pratiwi
+"""
+nama= input ("Winda Pratiwi")
+umur= 19
+
+#OUTPUT
+print("")
+print("=====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PYTHON=====")
+print("Winda Pratiwi", nama)
+print("19", umur)
+print("")
