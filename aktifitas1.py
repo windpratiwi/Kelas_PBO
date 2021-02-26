2cee4137bd852a04f32c33bd101402c7068e998f
windpratiwi <79292299+windpratiwi@users.noreply.github.com>
2021-02-26 10:19:03 +0700

    Create aktifitas1.py

--- aktifitas1.py
+++ aktifitas1.py
@@ -4,12 +4,12 @@ Created on Fri Feb 26 09:06:44 2021
 
 @author: Winda Pratiwi
 """
-nama= input ("Winda Pratiwi")
+nama= input ("Masukkan Nama Anda: ")
 umur= 19
 
 #OUTPUT
 print("")
 print("=====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PYTHON=====")
-print("Winda Pratiwi", nama)
-print("19", umur)
+print("Nama Anda: ", nama)
+print("Umur Anda:", umur)
 print("")
