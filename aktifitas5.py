131369646cd5baa3f7f881da4e190d407f49eadb
Winda Pratiwi <windapratiwi12@gmail.com>
2021-02-26 10:28:20 +0700

    Menambah File aktifitas5.py

---
+++ aktifitas5.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 10:24:21 2021
+
+@author: Winda Pratiwi
+"""
+
+x = int (input("Masukkan Tinggi: "))
+y = 1
+
+while(y<=x):
+    print ("*" * (y))
+    y=y+1
